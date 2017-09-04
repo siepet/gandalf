d156f2f385e8aa9741e78922c86ceaf29d20c51c
Marcin Siepetowski <marcin.siepetowski@netguru.pl>
2017-09-04 20:33:32 +0200

    add tests to handle form submit

--- test/gandalf_test.exs
+++ test/gandalf_test.exs
@@ -22,4 +22,32 @@ defmodule GandalfTest do
 
     assert conn.state == :unset
   end
+
+  test "returns conn when valid auth_key provided in form" do
+    conn = conn(:post, "/", code: "1234")
+
+    conn = Gandalf.call(conn, @opts)
+
+    assert conn.state == :sent
+  end
+
+  test "sets proper cookie if valid auth key " do
+    conn = conn(:post, "/", code: "1234")
+
+    conn = Gandalf.call(conn, @opts)
+
+    conn = fetch_cookies(conn)
+
+    assert conn.cookies["auth_key"] == "1234"
+  end
+
+  test "renders form when invalid auth key sent in form" do
+    conn = conn(:post, "/", code: "invalid")
+
+    conn = Gandalf.call(conn, @opts)
+
+    assert conn.state == :sent
+    assert conn.status == 200
+    assert conn.resp_body =~ "Sign in"
+  end
 end
